64a3333e33bb7e12cc88a89f74c1e33a2e3efe3f
pjenkinson <pjenkinson@knowsleycollege.ac.uk>
2016-11-04 16:42:42 +0000

    Theme update

--- page-apply.php
+++ page-apply.php
@@ -658,42 +658,6 @@ $sql = "SELECT HeardAboutCollegeID, Description
 	}
 
 	session_destroy();
-
-	//@TODO - SEND EMAIL HERE...
-/* (require_once('phpmailer/PHPMailerAutoload.php');
-
-$mail = new PHPMailer;
-
-//$mail->SMTPDebug = 3;                               // Enable verbose debug output
-
-$mail->isSMTP();                                      // Set mailer to use SMTP
-$mail->SMTPDebug = 2;
-$mail->Host = 'smtp.office365.com';  // Specify main and backup SMTP servers
-$mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'applications@knowsleycollege.ac.uk';                 // SMTP username
-$mail->Password = 'Appl1cat10n5';                          // SMTP password
-$mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
-$mail->Port = 587;                                    // TCP port to connect to
-
-$mail->setFrom('applications@knowsleycollege.ac.uk', 'Knowsley Community College');
-$mail->addAddress('pjenkinsonmail@gmail.com', 'Paul Jenkinson');     // Add a recipient
-$mail->addReplyTo('info@example.com', 'Information');
-$mail->addCC('cc@example.com');
-$mail->addBCC('bcc@example.com');
-
-$mail->isHTML(true);                                  // Set email format to HTML
-
-$mail->Subject = 'Here is the subject';
-$mail->Body    = 'This is the HTML message body <b>in bold!</b>';
-$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
-
-if(!$mail->send()) {
-    echo 'Message could not be sent.';
-    echo 'Mailer Error: ' . $mail->ErrorInfo;
-} else {
-    echo 'Message has been sent';
-}
-*/
 	
 	?>
 	<div class="the-content">
