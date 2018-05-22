3a9b234edd4d2f95579691e29d7b04dab2a076ce
pjenkinson <pjenkinson@knowsleycollege.ac.uk>
2018-05-22 17:15:53 +0100

    Theme update

--- footer/class.php
+++ footer/class.php
@@ -1,2 +1,8 @@
-<?php if( get_field('branding') == 'KCC Primary' ): ?>kcc-primary	<?php endif; ?><?php if( get_field('branding') == 'Higher Education' ): ?>higher-education<?php endif; ?><?php if( get_field('branding') == 'Employers' ): ?>employers<?php endif; ?><?php if( get_field('branding') == 'Lee Stafford' ): ?>lee-stafford<?php endif; ?><?php if( get_field('branding') == 'The Bistro' ): ?>the-bistro<?php endif; ?><?php if( get_field('branding') == 'WorkWorld' ): ?>workworld<?php endif; ?><?php if( get_field('branding') == 'IAMT' ): ?>iamt<?php endif; ?><?php if( get_field('branding') == 'KCC Live' ): ?>kcc-live<?php endif; ?><?php if( get_field('branding') == 'Studio 115' ): ?>studio-115<?php endif; ?><?php if( get_field('branding') == 'Northern Logistics Academy' ): ?>nla<?php endif; ?>
+<?php if( get_field('branding') == 'KCC Primary' ): ?>kcc-home	<?php endif; ?>
+<?php if( get_field('branding') == 'Employers' ): ?>employers<?php endif; ?>
+<?php if( get_field('branding') == 'Lee Stafford' ): ?>lee-stafford<?php endif; ?>
+<?php if( get_field('branding') == 'The Bistro' ): ?>the-bistro<?php endif; ?>
+<?php if( get_field('branding') == 'WorkWorld' ): ?>workworld<?php endif; ?>
+<?php if( get_field('branding') == 'IAMT' ): ?>iamt<?php endif; ?>
+<?php if( get_field('branding') == 'Northern Logistics Academy' ): ?>nla<?php endif; ?>
 <?php if ( is_home() ) {?>kcc-home<?php } else {}?>
